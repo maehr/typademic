cf970ae172fc3e4b54d71fe60e5e25945d3d49d5
Moritz Maehr <moritz.maehr@gmail.com>
2018-07-06 23:40:46 +0200

    app

--- app.py
+++ app.py
@@ -10,6 +10,17 @@ server = app.server
 
 app.css.append_css({"external_url": "https://codepen.io/chriddyp/pen/bWLwgP.css"})
 
+markdown_text = '''
+### Dash and Markdown
+
+Dash apps can be written in Markdown.
+Dash uses the [CommonMark](http://commonmark.org/)
+specification of Markdown.
+Check out their [60 Second Markdown Tutorial](http://commonmark.org/help/)
+if this is your first introduction to Markdown!
+'''
+
+
 app.layout = html.Div([
     html.H2('Hello World'),
     dcc.Dropdown(
@@ -17,13 +28,29 @@ app.layout = html.Div([
         options=[{'label': i, 'value': i} for i in ['LA', 'NYC', 'MTL']],
         value='LA'
     ),
-    html.Div(id='display-value')
+    html.Div(id='display-value'),
+    dcc.Markdown(children=markdown_text)
+    # dcc.Upload
+    # html.Div(dcc.Input(id='input-box', type='text')),
+    # html.Button('Submit', id='button'),
+    # html.Div(id='output-container-button',
+    #          children='Enter a value and press submit')
 ])
 
 @app.callback(dash.dependencies.Output('display-value', 'children'),
               [dash.dependencies.Input('dropdown', 'value')])
 def display_value(value):
     return 'You have selected "{}"'.format(value)
+
+# @app.callback(
+#     dash.dependencies.Output('output-container-button', 'children'),
+#     [dash.dependencies.Input('button', 'n_clicks')],
+#     [dash.dependencies.State('input-box', 'value')])
+# def update_output(n_clicks, value):
+#     return 'The input value was "{}" and the button has been clicked {} times'.format(
+#         value,
+#         n_clicks
+#     )
 
 if __name__ == '__main__':
     app.run_server(debug=True)
