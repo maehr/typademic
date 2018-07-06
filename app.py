fd0847f3806e3b7485f514d2ef12e5b3a54b4bdf
Moritz Maehr <moritz.maehr@gmail.com>
2018-07-06 21:17:44 +0200

    dash example app
    https://dash.plot.ly/deployment

---
+++ app.py
@@ -0,0 +1,29 @@
+import os
+
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+
+
+app = dash.Dash(__name__)
+server = app.server
+
+app.css.append_css({"external_url": "https://codepen.io/chriddyp/pen/bWLwgP.css"})
+
+app.layout = html.Div([
+    html.H2('Hello World'),
+    dcc.Dropdown(
+        id='dropdown',
+        options=[{'label': i, 'value': i} for i in ['LA', 'NYC', 'MTL']],
+        value='LA'
+    ),
+    html.Div(id='display-value')
+])
+
+@app.callback(dash.dependencies.Output('display-value', 'children'),
+              [dash.dependencies.Input('dropdown', 'value')])
+def display_value(value):
+    return 'You have selected "{}"'.format(value)
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
