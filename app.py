c6ed452c438b49b64e00370ccc4e339d2eb798f4
Moritz Maehr <moritz.maehr@gmail.com>
2018-07-07 10:09:25 +0200

    sh added to use pandoc, app upload template

--- app.py
+++ app.py
@@ -1,16 +1,24 @@
+# https://medium.com/@plotlygraphs/introducing-dash-5ecf7191b503
+
 import os
 
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+from sh import pandoc
+print(pandoc("-v"))
+# print(pandoc("arg1, "arg2"))
+# https://amoffat.github.io/sh/
 
 
 app = dash.Dash(__name__)
 server = app.server
 
-app.css.append_css({"external_url": "https://codepen.io/chriddyp/pen/bWLwgP.css"})
+app.css.append_css({'external_url': 'https://codepen.io/chriddyp/pen/bWLwgP.css'})
+# app.css.config.serve_locally = True
+# app.scripts.config.serve_locally = True
 
-markdown_text = '''
+step_1 = '''
 ### Dash and Markdown
 
 Dash apps can be written in Markdown.
@@ -20,16 +28,49 @@ Check out their [60 Second Markdown Tutorial](http://commonmark.org/help/)
 if this is your first introduction to Markdown!
 '''
 
+step_2 = '''
+### Dash and Markdown
+
+Dash apps can be written in Markdown.
+Dash uses the [CommonMark](http://commonmark.org/)
+specification of Markdown.
+Check out their [60 Second Markdown Tutorial](http://commonmark.org/help/)
+if this is your first introduction to Markdown!
+'''
+
+step_3 = '''
+### Dash and Markdown
+
+Dash apps can be written in Markdown.
+Dash uses the [CommonMark](http://commonmark.org/)
+specification of Markdown.
+Check out their [60 Second Markdown Tutorial](http://commonmark.org/help/)
+if this is your first introduction to Markdown!
+'''
 
 app.layout = html.Div([
-    html.H2('Hello World'),
+    dcc.Markdown(children=step_1),
+    dcc.Markdown(children=step_2),
+    dcc.Markdown(children=step_3),
+    dcc.Upload([
+        'Drag and Drop or ',
+        html.A('Select a File')
+    ], style={
+        'width': '100%',
+        'height': '60px',
+        'lineHeight': '60px',
+        'borderWidth': '1px',
+        'borderStyle': 'dashed',
+        'borderRadius': '5px',
+        'textAlign': 'center'
+    }),
     dcc.Dropdown(
         id='dropdown',
         options=[{'label': i, 'value': i} for i in ['LA', 'NYC', 'MTL']],
         value='LA'
     ),
     html.Div(id='display-value'),
-    dcc.Markdown(children=markdown_text)
+    html.Button('Submit', id='button'),
     # dcc.Upload
     # html.Div(dcc.Input(id='input-box', type='text')),
     # html.Button('Submit', id='button'),
@@ -37,10 +78,12 @@ app.layout = html.Div([
     #          children='Enter a value and press submit')
 ])
 
+
 @app.callback(dash.dependencies.Output('display-value', 'children'),
               [dash.dependencies.Input('dropdown', 'value')])
 def display_value(value):
     return 'You have selected "{}"'.format(value)
+
 
 # @app.callback(
 #     dash.dependencies.Output('output-container-button', 'children'),
