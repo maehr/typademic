c4045fda15a27fa7241f302e2dbb5624d638fc13
Moritz Maehr <moritz.maehr@gmail.com>
2018-07-20 11:28:04 +0200

    refactor dash with simple flask

--- app.py
+++ app.py
@@ -1,99 +1,47 @@
-# https://medium.com/@plotlygraphs/introducing-dash-5ecf7191b503
-
+# -*- coding: utf-8 -*-
 import os
 
-import dash
-import dash_core_components as dcc
-import dash_html_components as html
+from flask import Flask, render_template, request
+from flask_dropzone import Dropzone
+from flask_wtf.csrf import CSRFProtect, CSRFError
 from sh import pandoc
-print(pandoc("-v"))
-# print(pandoc("arg1, "arg2"))
-# https://amoffat.github.io/sh/
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+app = Flask(__name__)
+
+app.config.update(
+    SECRET_KEY=os.getenv('SECRET_KEY', '6*1cu#b!r)0oonlbq(ed-()7kw76d_ent93az*(o313l6es*!c'),
+    # the secret key used to generate CSRF token
+    UPLOADED_PATH=os.path.join(basedir, 'uploads'),
+    # Flask-Dropzone config:
+    DROPZONE_ALLOWED_FILE_CUSTOM=True,
+    DROPZONE_ALLOWED_FILE_TYPE='image/*, text/*, .bib, .bibtex, .biblatex, .csl, .yaml, .yml, .md, .markdown, .Rmd, .json',
+    DROPZONE_MAX_FILE_SIZE=10,
+    DROPZONE_MAX_FILES=30,
+    DROPZONE_ENABLE_CSRF=True  # enable CSRF protection
+)
+
+dropzone = Dropzone(app)
+csrf = CSRFProtect(app)  # initialize CSRFProtect
 
 
-app = dash.Dash(__name__)
-server = app.server
-
-app.css.append_css({'external_url': 'https://codepen.io/chriddyp/pen/bWLwgP.css'})
-# app.css.config.serve_locally = True
-# app.scripts.config.serve_locally = True
-
-step_1 = '''
-### Dash and Markdown
-
-Dash apps can be written in Markdown.
-Dash uses the [CommonMark](http://commonmark.org/)
-specification of Markdown.
-Check out their [60 Second Markdown Tutorial](http://commonmark.org/help/)
-if this is your first introduction to Markdown!
-'''
-
-step_2 = '''
-### Dash and Markdown
-
-Dash apps can be written in Markdown.
-Dash uses the [CommonMark](http://commonmark.org/)
-specification of Markdown.
-Check out their [60 Second Markdown Tutorial](http://commonmark.org/help/)
-if this is your first introduction to Markdown!
-'''
-
-step_3 = '''
-### Dash and Markdown
-
-Dash apps can be written in Markdown.
-Dash uses the [CommonMark](http://commonmark.org/)
-specification of Markdown.
-Check out their [60 Second Markdown Tutorial](http://commonmark.org/help/)
-if this is your first introduction to Markdown!
-'''
-
-app.layout = html.Div([
-    dcc.Markdown(children=step_1),
-    dcc.Markdown(children=step_2),
-    dcc.Markdown(children=step_3),
-    dcc.Upload([
-        'Drag and Drop or ',
-        html.A('Select a File')
-    ], style={
-        'width': '100%',
-        'height': '60px',
-        'lineHeight': '60px',
-        'borderWidth': '1px',
-        'borderStyle': 'dashed',
-        'borderRadius': '5px',
-        'textAlign': 'center'
-    }),
-    dcc.Dropdown(
-        id='dropdown',
-        options=[{'label': i, 'value': i} for i in ['LA', 'NYC', 'MTL']],
-        value='LA'
-    ),
-    html.Div(id='display-value'),
-    html.Button('Submit', id='button'),
-    # dcc.Upload
-    # html.Div(dcc.Input(id='input-box', type='text')),
-    # html.Button('Submit', id='button'),
-    # html.Div(id='output-container-button',
-    #          children='Enter a value and press submit')
-])
+@app.route('/', methods=['POST', 'GET'])
+def upload():
+    if request.method == 'POST':
+        f = request.files.get('file')
+        f.save(os.path.join(app.config['UPLOADED_PATH'], f.filename))
+        print(pandoc("-v"))
+        # print(pandoc("arg1, "arg2"))
+        # https://amoffat.github.io/sh/
+    return render_template('index.html')
 
 
-@app.callback(dash.dependencies.Output('display-value', 'children'),
-              [dash.dependencies.Input('dropdown', 'value')])
-def display_value(value):
-    return 'You have selected "{}"'.format(value)
+# handle CSRF error
+@app.errorhandler(CSRFError)
+def csrf_error(e):
+    return e.description, 400
 
-
-# @app.callback(
-#     dash.dependencies.Output('output-container-button', 'children'),
-#     [dash.dependencies.Input('button', 'n_clicks')],
-#     [dash.dependencies.State('input-box', 'value')])
-# def update_output(n_clicks, value):
-#     return 'The input value was "{}" and the button has been clicked {} times'.format(
-#         value,
-#         n_clicks
-#     )
 
 if __name__ == '__main__':
-    app.run_server(debug=True)
+    app.run(debug=True)
