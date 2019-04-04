0a5523f37d73fccebae5bce293ea21829cc1c2d4
Moritz Mähr <moritz.maehr@gmail.com>
2019-04-04 11:15:28 +0200

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -48,7 +48,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'typademic'
-copyright = u"2019, Moritz Maehr"
+copyright = u"2018, Moritz Maehr"
 author = u"Moritz Maehr"
 
 # The version info for the project you're documenting, acts as replacement
