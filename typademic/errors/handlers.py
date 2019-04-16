95e9e54ddf27a87f0a51f42579022cac9a845e18
Moritz Mähr <moritz.maehr@gmail.com>
2019-04-16 22:35:13 +0200

    Update handlers.py

--- typademic/errors/handlers.py
+++ typademic/errors/handlers.py
@@ -11,7 +11,7 @@ def csrf_error(e):
 
 
 @blueprint.app_errorhandler(404)
-def not_found_error(e):
+def not_found_error(error):
     return render_template('errors/404.html'), 404
 
 
@@ -21,5 +21,5 @@ def ratelimit_handler(e):
 
 
 @blueprint.app_errorhandler(500)
-def internal_error(e):
+def internal_error(error):
     return render_template('errors/500.html'), 500
