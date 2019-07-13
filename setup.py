1ebf8bd1aaa836323af3f99a09bb8ef2e17eb498
Moritz Mähr <moritz.maehr@gmail.com>
2019-07-13 12:15:58 +0200

    Revert "Bump version: 1.2.1 → 1.3.0"
    This reverts commit 77298201b14b6c00e12f91ae726874934d1a49e2.

--- setup.py
+++ setup.py
@@ -52,6 +52,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/maehr/typademic',
-    version='1.3.0',
+    version='1.2.1',
     zip_safe=False,
 )
