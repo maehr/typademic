1610eba516483511dcded6f2624205eae8e9cfdb
Moritz Mähr <moritz.maehr@gmail.com>
2019-07-13 12:15:44 +0200

    Bump version: 1.2.1 → 1.3.0

--- setup.py
+++ setup.py
@@ -52,6 +52,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/maehr/typademic',
-    version='1.2.1',
+    version='1.3.0',
     zip_safe=False,
 )
